26cfb1f15a11585394d3fd6a75fcbbd696d52aac
LarryByrd89 <laryn.n99@gmail.com>
2023-05-04 22:17:24 -0400

    connected arrays, deleted unnecessary sections

--- Develop/script.js
+++ Develop/script.js
@@ -2,51 +2,50 @@
 function generatePassword() {
   console.log("Generate Password button clicked")
 
-    var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
-    var numbersChar = "1234567890";
-    var specialChar = "!@#$%^&*()-_+={}[],.?/:;\|";
+    var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+    var lowercaseChar = "abcdefghijklmnopqrstuvwxyz".split("")
+    var numbersChar = "1234567890".split("")
+    var specialChar = "!@#$%^&*()-_+={}[],.?/:;\|".split("")
 
     let passwordLength = parseInt(prompt('How many characters do you want in your password? (Enter a number between 8 and 128)'));
   
-    
-   
     
     const includeLowercase = confirm('Do you want to include lowercase letters?');
     const includeUppercase = confirm('Do you want to include uppercase letters?');
     const includeNumeric = confirm('Do you want to include numbers?');
     const includeSpecial = confirm('Do you want to include special characters?');
     
-    
+    var possibleChar = []
     if (includeLowercase) {
-      possibleChars = possibleChars.concat(lowercaseChars);
+      possibleChar = possibleChar.concat(lowercaseChar);
     }
     if (includeUppercase) {
-      possibleChars = possibleChars.concat(uppercaseChars);
+      possibleChar = possibleChar.concat(uppercaseChar);
     }
     if (includeNumeric) {
-      possibleChars = possibleChars.concat(numericChars);
+      possibleChar = possibleChar.concat(numbersChar);
     }
     if (includeSpecial) {
-      possibleChars = possibleChars.concat(specialChars);
+      possibleChar = possibleChar.concat(specialChar);
     }
     
     
     var password = '';
     for (var i = 0; i < passwordLength; i++) {
-      var index = Math.floor(Math.random() * options.length);
-    var computerChoice = options[index];
+      var index = Math.floor(Math.random() * possibleChar.length);
+      var computerChoice = possibleChar[index];
+      password = password.concat(computerChoice)
     }
     
-    for (let x = 0; x < passwordLength; x++) {
-        let randomList = randomListCharacter(userChoices);
-        let randomCharacter = randomListCharacter (randomList);
-        generatePassword += randomCharacter;
-    }
+    // for (let x = 0; x < passwordLength; x++) {
+    //     let randomList = randomListCharacter(userChoices);
+    //     let randomCharacter = randomListCharacter (randomList);
+    //     generatePassword += randomCharacter;
+    // }
     return password;
   }
   
-  alert('Your new password is: ' + generatePassword());
+  // alert('Your new password is: ' + generatePassword());
 
   // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
